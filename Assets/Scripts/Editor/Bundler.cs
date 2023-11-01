93181f8de7a1293faa233cd5f78cb269f99ad3d6
Tryston Minsquero <tam190005@utdallas.edu>
2023-11-01 11:39:30 -0500

    Add menu context to choose asset bundles

--- Assets/Scripts/Editor/Bundler.cs
+++ Assets/Scripts/Editor/Bundler.cs
@@ -7,18 +7,39 @@ using UnityEngine.Assertions.Must;
 public class Bundler
 {
     
-    [MenuItem("Assets/Build AssetBundles")]
+    [MenuItem("Assets/Build AssetBundles/iOS and Android")]
     static void BuildAllAssetBundles()
+    {
+        BuildAllAssetBundles(BuildTarget.Android);
+        BuildAllAssetBundles(BuildTarget.iOS);
+    }
+
+    [MenuItem("Assets/Build AssetBundles/Android")]
+    static void BuildAndroidAssetBundles() => BuildAllAssetBundles(BuildTarget.Android);
+
+    [MenuItem("Assets/Build AssetBundles/iOS")]
+    static void BuildIOSAssetBundles() => BuildAllAssetBundles(BuildTarget.iOS);
+    
+    [MenuItem("Assets/Build AssetBundles/Windows")]
+    static void BuildWindowsAssetBundles() => BuildAllAssetBundles(BuildTarget.StandaloneWindows);
+    
+    static void BuildAllAssetBundles(BuildTarget target)
     {
         string assetBundleDirectory = "Assets/AssetBundles";
         if(!Directory.Exists(assetBundleDirectory))
         {
             Directory.CreateDirectory(assetBundleDirectory);
         }
+
+        assetBundleDirectory += $"/{target.ToString()}";
+        if(!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        
         BuildPipeline.BuildAssetBundles(assetBundleDirectory, 
             BuildAssetBundleOptions.None, 
-            BuildTarget.Android);
-        
+            target);
     }
 
     [MenuItem("Assets/LoadAssetBundle")]
@@ -35,5 +56,7 @@ public class Bundler
             }
         }
     }
+    
+    
     
 }
